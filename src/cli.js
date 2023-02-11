d68feff646acb4f485f4be0749423a34b1c6e7ce
VladyBarvy <vladbarskoff@yandex.ru>
2023-02-11 22:02:46 +0300

    step 10 part 1

--- src/cli.js
+++
@@ -1,7 +0,0 @@
-import readlineSync from 'readline-sync';
-const whatIsYourName = () => {
-    var userName = readlineSync.question('May I have your name? ');
-    console.log('Hello, ' + userName + '!');
-};
-
-export { whatIsYourName };
