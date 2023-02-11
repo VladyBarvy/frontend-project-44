78e0f5f5d8acd387381a43db661dd709d98a19bb
VladyBarvy <vladbarskoff@yandex.ru>
2023-02-11 22:08:09 +0300

    step 10 part 2

---
+++ bin/brain-games.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+/* eslint-disable */
+import { whatIsYourName } from '/home/barvv/workout/frontend-project-44/src/cli.js';
+/* eslint-enable */
+
+/* eslint-disable */
+console.log("Welcome to the Brain Games!");
+/* eslint-enable */
+whatIsYourName();
